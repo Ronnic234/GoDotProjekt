78d1f7955d8ba9f3adf1d661511dd59f49995514
LucaCn1 <96244881+LucaCn1@users.noreply.github.com>
2022-02-10 13:49:01 +0100

    hhhhh

--- scripts/Aufklärung_Text.gd
+++ scripts/Aufklärung_Text.gd
@@ -2,7 +2,8 @@ extends ColorRect
 
 # Der Text zur Aufklärung soll versteckt sein, wenn der Spieler kein Konkat mit dem Feld hat 
 # Von Fabian als Test
-# OK Find ich gut!
+# OK Find ich gut!hhhhhh
+
 func _ready():
 	self.hide()
 
