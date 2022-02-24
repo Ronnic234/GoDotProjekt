4cb74dadf5e1f2dc0cd787e76026f5e5502c9cbf
Ronnic234 <96244768+Ronnic234@users.noreply.github.com>
2022-02-24 14:52:18 +0100

    24.2.22
    Gegner klappen nnicht

--- scripts/SpielStarten.gd
+++ scripts/SpielStarten.gd
@@ -5,7 +5,8 @@ extends Button
 
 #Wenn der Knopf "Spiel Starten" gedrückt wird, wird das Spiel gestartet und man landet in World 1 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Welt 3.tscn")
+	get_tree().change_scene("res://World_2.tscn")
+	
 	
 	
 	
